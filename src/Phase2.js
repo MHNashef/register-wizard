2f73d71b896911cb10edcd0744f81ffc8b344da6
Hisham Nashef <hishamnshf@gmail.com>
2021-06-01 00:22:58 +0300

    formated code

--- src/Phase2.js
+++ src/Phase2.js
@@ -1,93 +1,108 @@
-import React, {useState} from "react";
-import {Form, Container, Button} from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Container, Button } from "react-bootstrap";
 
 function Phase2() {
-    const [emptyCityError, setEmptyCityError] = useState(null);
-    const [emptyStreetError, setEmptyStreetError] = useState(null);
-    const [validation, setValidation] = useState({
-        validCity: false,
-        validStreet: false
-    });
-    const [userInfo, setUserInfo] = useState({
-        city: "",
-        street: "",
-        number: ""
-    });
+  const [emptyCityError, setEmptyCityError] = useState(null);
+  const [emptyStreetError, setEmptyStreetError] = useState(null);
+  const [validation, setValidation] = useState({
+    validCity: false,
+    validStreet: false,
+  });
+  const [userInfo, setUserInfo] = useState({
+    city: "",
+    street: "",
+    number: "",
+  });
 
-    function checkEmpty({target:{value, name}}) {
-        if (name === "City") {
-            if (value.length === 0) {
-                setEmptyCityError(`${name} is required`);
-                validation.validCity = false;
-            } else {
-                setEmptyCityError("");
-                validation.validCity = true;
-                userInfo.city = value;
-            }
-        } else if (name === "Street") {
-            if (value.length === 0) {
-                setEmptyStreetError(`${name} is required`);
-                validation.validStreet = false;
-            } else {
-                setEmptyStreetError("");
-                validation.validStreet = true;
-                userInfo.street = value;
-            }
-        }
+  function checkEmpty({ target: { value, name } }) {
+    if (name === "City") {
+      if (value.length === 0) {
+        setEmptyCityError(`${name} is required`);
+        validation.validCity = false;
+      } else {
+        setEmptyCityError("");
+        validation.validCity = true;
+        userInfo.city = value;
+      }
+    } else if (name === "Street") {
+      if (value.length === 0) {
+        setEmptyStreetError(`${name} is required`);
+        validation.validStreet = false;
+      } else {
+        setEmptyStreetError("");
+        validation.validStreet = true;
+        userInfo.street = value;
+      }
     }
+  }
 
-    function updateNumValue({target:{value}}) {
-        userInfo.number = value;
+  function updateNumValue({ target: { value } }) {
+    userInfo.number = value;
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+  }
+
+  function saveInfo() {
+    console.log(userInfo);
+    if (validation["validCity"] && validation["validStreet"]) {
+      window.localStorage.setItem("user", JSON.stringify(userInfo));
     }
+  }
 
-    function onSubmit(e) {
-        e.preventDefault();
-    }
+  return (
+    <>
+      <Container>
+        <Form onSubmit={onSubmit}>
+          <Form.Group controlId="formBasicCity">
+            <Form.Label>City</Form.Label>
+            <Form.Control
+              type="text"
+              name="City"
+              placeholder="Enter city"
+              onBlur={checkEmpty}
+            />
+            {emptyCityError && (
+              <Form.Text className="text-danger">{emptyCityError}</Form.Text>
+            )}
+          </Form.Group>
 
-    function saveInfo() {
-        console.log(userInfo);
-        if (validation["validCity"] && validation["validStreet"]) {
-            window.localStorage.setItem("user", JSON.stringify(userInfo));
-        } 
-    }
+          <Form.Group controlId="formBasicStreet">
+            <Form.Label>Street</Form.Label>
+            <Form.Control
+              type="text"
+              name="Street"
+              placeholder="Street"
+              onBlur={checkEmpty}
+            />
+            {emptyStreetError && (
+              <Form.Text className="text-danger">{emptyStreetError}</Form.Text>
+            )}
+          </Form.Group>
 
-    return (
-        <>
-            <Container>
-                <Form onSubmit={onSubmit}>
-                    <Form.Group controlId="formBasicCity">
-                        <Form.Label>City</Form.Label>
-                        <Form.Control type="text" name="City" placeholder="Enter city" onBlur={checkEmpty}/>
-                        {
-                            emptyCityError &&
-                            <Form.Text className="text-danger">{emptyCityError}</Form.Text>
-                        }
-                    </Form.Group>
-
-                    <Form.Group controlId="formBasicStreet">
-                        <Form.Label>Street</Form.Label>
-                        <Form.Control type="text" name="Street" placeholder="Street" onBlur={checkEmpty}/>
-                        {
-                            emptyStreetError &&
-                            <Form.Text className="text-danger">{emptyStreetError}</Form.Text>
-                        }
-                    </Form.Group>
-
-                    <Form.Group controlId="formBasicNumber">
-                        <Form.Label>Number</Form.Label>
-                        <Form.Control type="number" min="1" max="999" name="Number" placeholder="Number" onBlur={updateNumValue}/>
-                        {/* {
+          <Form.Group controlId="formBasicNumber">
+            <Form.Label>Number</Form.Label>
+            <Form.Control
+              type="number"
+              min="1"
+              max="999"
+              name="Number"
+              placeholder="Number"
+              onBlur={updateNumValue}
+            />
+            {/* {
                             invalidNumError &&
                             <Form.Text className="text-danger">{invalidNumError}</Form.Text>
                         } */}
-                    </Form.Group>
-                </Form>
-                <Button variant="primary" type="submit" onClick={saveInfo}>
-                    Submit
-                </Button>
-            </Container>
-        </>
-    );
+          </Form.Group>
+        </Form>
+        <Button variant="primary" type="submit" onClick={saveInfo}>
+          Submit
+        </Button>
+      </Container>
+    </>
+  );
 }
 
 export default Phase2;
