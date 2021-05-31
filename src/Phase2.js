714a1e55b58afa7d49c066c0f89cefbb5c9293c7
Hisham Nashef <hishamnshf@gmail.com>
2021-05-31 16:25:29 +0300

    added react form using reactBootstrap

---
+++ src/Phase2.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Form, Container} from 'react-bootstrap';
+
+function Phase2() {
+
+    return (
+        <>
+            <Container>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        {/* <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text> */}
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                </Form>
+            </Container>
+        </>
+    );
+}
+
+export default Phase2;
