288da0dab9922722deb6595d90a322258504e265
Hisham Nashef <hishamnshf@gmail.com>
2021-06-03 02:12:32 +0300

    added badges to success card

--- src/completionPage/CompletionPage.jsx
+++ src/completionPage/CompletionPage.jsx
@@ -1,36 +1,65 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Jumbotron, Card, Button } from "react-bootstrap";
+import { Card, Button, Badge } from "react-bootstrap";
 
 export default function CompletionPage({ userCnt, setUserCnt }) {
   const history = useHistory();
-  const userDetails = JSON.parse(window.localStorage.getItem(`user_${userCnt}`));
+  const userDetails = JSON.parse(
+    window.localStorage.getItem(`user_${userCnt}`)
+  );
 
   function returnHome() {
-    setUserCnt(prev => ++prev);
+    setUserCnt((prev) => ++prev);
     history.push("/");
   }
 
   return (
     <>
-      <div className="position">
+      <div className="position mt-5">
         <h1 className="text-center">Registration Complete!</h1>
       </div>
-      <Card className="mx-auto registration-card" style={{ width: '25rem' }}>
+      <Card className="mx-auto registration-card" style={{ width: "25rem" }}>
         <Card.Img variant="top" src={`${userDetails.imgURL}`} />
         <Card.Body>
-          <Card.Title>Your Details - </Card.Title>
+          <Card.Title>Your Details </Card.Title>
           <Card.Text>
-            <p>{`Full Name: ${userDetails.name}`}</p>
-            <p>{`Email: ${userDetails.Email}`}</p>
-            <p>{`Birthdate: ${userDetails.birthDate.split("-").reverse().join("-")}`}</p>
-            <p>{`Address: ${userDetails.city}, ${userDetails.street} ${userDetails.number}`}</p>
-            <p>{`Hobbies: ${userDetails.hobbies}`}</p>
+            <h6>
+              <Badge pill className="badge-bg-color mr-2">
+                Full Name:
+              </Badge>
+              {userDetails.name}
+            </h6>
+            <h6>
+              <Badge pill className="badge-bg-color mr-2">
+                Email:
+              </Badge>
+              {userDetails.Email}
+            </h6>
+            <h6>
+              <Badge pill className="badge-bg-color mr-2">
+                Birthdate:
+              </Badge>
+              {userDetails.birthDate.split("-").reverse().join("-")}
+            </h6>
+            <h6>
+              <Badge pill className="badge-bg-color mr-2">
+                Address:
+              </Badge>
+              {`${userDetails.city}, ${userDetails.street} ${userDetails.number}`}
+            </h6>
+            <h6>
+              <Badge pill className="badge-bg-color mr-2">
+                Hobbies:
+              </Badge>
+              {userDetails.hobbies}
+            </h6>
           </Card.Text>
         </Card.Body>
       </Card>
       <div className="text-center mt-2">
-      <Button className="buttonColor" onClick={returnHome}>Add New User</Button>
+        <Button className="buttonColor" onClick={returnHome}>
+          Add New User
+        </Button>
       </div>
     </>
   );
